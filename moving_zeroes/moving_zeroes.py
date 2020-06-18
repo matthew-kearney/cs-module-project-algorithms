a5b69c1220ed27f6cab7a6c372834dad8acdf1c9
Matthew Kearney <matthewkearney@matthews-mbp.lan>
2020-06-18 15:25:25 -0500

    4/5

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,8 +4,22 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
+    # we need to move all the zeroes to the end of our list
+    # loop through the list, if a the number is 0 
+    # put the zero at the end of the list 
+    # do this until all zeroes goes to the end of the list
+    
+    
+    # if count = 0 non zero numbers
+    count = arr.count(0)
+    # for every i in range of count
+    for i in range(count):
+        # add the 0 to the arr
+        arr.append(0)
+        # or remove 0 from the array
+        arr.remove(0)
+    return arr
 
-    pass
 
 
 if __name__ == '__main__':
